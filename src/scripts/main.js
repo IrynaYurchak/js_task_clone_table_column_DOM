88d00669d1194ce79077e99161c11b5561071525
Iryna <Yurchak.Iryna2017@gmail.com>
2025-09-30 14:30:48 +0300

    New solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,15 @@
 const table = document.querySelector('table');
 
 for (const row of table.rows) {
-  const secondCell = row.cells[1];
-  const clone = secondCell.cloneNode(true);
+  if (row.cells.length < 2) {
+    continue;
+  }
 
-  row.insertBefore(clone, row.cells[row.cells.length - 1]);
+  const clone = row.cells[1].cloneNode(true);
+  const lastCell = row.cells[row.cells.length - 1];
+
+  if (!lastCell) {
+    continue;
+  }
+  row.insertBefore(clone, lastCell);
 }
